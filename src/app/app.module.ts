362c4826a5c0a35248f2ff237af7b0d8233ca15c
Sebastien Morvan <smorvan@mail.ks.int>
2018-09-27 18:13:46 +0200

    Merge ionic and firebase

---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { ChatPage } from '../pages/chat/chat';
+import { TabsPage } from '../pages/tabs/tabs';
+
+import {ChatBubble} from '../components/chatBubble/chatBubble';
+import {ElasticTextarea} from '../components/elasticTextarea';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    ChatPage,
+    TabsPage,
+    ChatBubble,
+    ElasticTextarea
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    ChatPage,
+    TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
